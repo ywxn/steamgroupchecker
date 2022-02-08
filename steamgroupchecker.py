684ec5438fc31820f53d6723d8196fd337094fcd
jeanne <91235043+jeannegit@users.noreply.github.com>
2022-02-07 18:26:30 -0800

    main file

---
+++ steamgroupchecker.py
@@ -0,0 +1,43 @@
+"""
+Simple python script designed to test the whether steam groups are OG or not.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+def unicodeCheck(groupName,groupTag):
+    nameVariable = 0
+    tagVariable = 0
+    for characterName in groupName:
+        if ord(characterName) > 127:
+            nameVariable = nameVariable + 1
+    for characterTag in groupTag:
+        if ord(characterTag) > 127:
+            tagVariable = tagVariable + 1
+    if nameVariable > 0:
+        print("The group name contains unicode.")
+    else:
+        print("The group name does not contain unicode.")
+    if tagVariable > 0:
+        print("The group tag contains unicode.")
+    else:
+        print("The group tag does not contain unicode.")
+    
+
+def main():
+    while True:
+        url = input("Enter the url of the steam group here: ")
+        content = requests.get(url)
+        soup = BeautifulSoup(content.content, 'html.parser')
+        groupName = soup.select_one(".grouppage_header_name").text
+        groupTag = soup.select_one(".grouppage_header_name > span:nth-child(1)").text
+        unicodeCheck(groupName,groupTag)
+        yesNo = input("Do you want to test another group (yes/no)? ")
+        while yesNo.lower() not in ("yes","no"):
+            yesNo = input("Do you want to test another group(yes/no)? ")
+        if yesNo == "no":
+            break
+
+
+if __name__ == "__main__":
+    main()
